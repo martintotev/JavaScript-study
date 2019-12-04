6daf76179437e957dc7e68362ba00b4c81151d1e
Martin Totev <martintotev89@gmail.com>
2019-12-04 23:37:04 +0900

    S2 - part 01
    Basics of JavaScript, what is JavaScript, etc.

---
+++ S2-basics/script.js
@@ -0,0 +1,19 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(age);
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// var 3years = 3; can't use a number to name a var
+// can't use symbols in names
+// can;t use delete, if and other JS essential words
