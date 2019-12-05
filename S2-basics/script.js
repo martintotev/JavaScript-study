e9f95b74ca960142aace2b1ca85b6eb5523cc1a9
Martin Totev <martintotev89@gmail.com>
2019-12-05 23:46:36 +0900

    S2 - part 01

--- S2-basics/script.js
+++ S2-basics/script.js
@@ -8,12 +8,30 @@ var fullAge = true;
 console.log(age);
 console.log(fullAge);
 
+/*
 var job;
 console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
 
 // var 3years = 3; can't use a number to name a var
 // can't use symbols in names
 // can;t use delete, if and other JS essential words
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
