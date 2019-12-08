ba11a59932c9f4c1c483123e673100ee89781f15
Martin Totev <martintotev89@gmail.com>
2019-12-08 22:27:24 +0900

    S2 - part 03
    Math and logical operations from lecture 10

---
+++ S2-basics/lecture10.js
@@ -0,0 +1,29 @@
+//Basic operators
+
+var yearJohn = 2018 - 28;
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+//Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+yearJohn = now - 28;
+yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//Logical operation
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//"typeof" operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is odler');
---
+++ S2-basics/lecture11.js
@@ -0,0 +1,8 @@
+//Operator precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
