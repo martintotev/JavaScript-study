acab54978ee1eaf2bdef5cb3649f68612e8d3ede
Martin Totev <martintotev89@gmail.com>
2019-12-11 23:18:48 +0900

    S2 - part 04
    Learned about "Operator Precedence" and about operators.

--- S2-basics/lecture11.js
+++ S2-basics/lecture11.js
@@ -6,3 +6,23 @@ var fullAge = 18;
 
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+var x, y;
+x = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+x = y = (3 + 5) * 4 - 6; // assignments are calculated from right to left
+// result = y, result = x
+console.log(x, y);
+
+//More operators
+x = x * 2;
+x *= 2;
+// same code, different way of writing it
+x += 10;
+x++; // same as x += 1; same as x = x + 1;
+
+console.log(x);
