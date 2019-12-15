69eac643716d294e80d33217b3d3e3ee5bf60968
Martin Totev <martintotev89@gmail.com>
2019-12-15 17:18:58 +0900

    Practice 04
    Practice if else statements with what to watch question.

---
+++ S2-basics/lecture15-practice.js
@@ -0,0 +1,14 @@
+var siliconValley, starTrekVoyager, theManInTheHighCastle;
+var toWatch = 'comedy';
+
+siliconValley = 'comedy';
+starTrekVoyager = 'sci-fi';
+theManInTheHighCastle = 'thriller';
+
+if (toWatch = 'comedy') {
+    console.log('You should watch Silicon Vallet');
+} else if (toWatch = 'sci-fi') {
+    console.log('You should watch Star Trek Voyager');
+} else {
+    console.log('You should watch The Man In The High Castle');
+}
