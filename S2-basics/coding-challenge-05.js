5bf67cfad915d9714d763f49a7e7b0809d01ec67
Martin Totev <martintotev89@gmail.com>
2020-01-04 20:18:32 +0900

    S2 - lecture 31 - Challenge solution p2
    How to do the extra part of the challenge.

--- S2-basics/coding-challenge-05.js
+++ S2-basics/coding-challenge-05.js
@@ -59,5 +59,53 @@ var john = {
 
 };
 
+var mark = {
+    fullName: 'Mark Smith',
+    bills: [77, 475, 110, 45],
+    calcTip: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            var percentage;
+            var bill = this.bills[i];
+
+            if (bill < 100) {
+                percentage = 0.2;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = 0.1;
+            } else {
+                percentage = 0.25;
+            }
+
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+
+};
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
 john.calcTip();
+mark.calcTip();
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+
 console.log(john);
+console.log(mark);
+
+if (john.average > mark.average) {
+    console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+} else {
+    console.log('Both ' + john.fullName + ' and ' + mark.fullName + ' pay the same tips.');
+}
